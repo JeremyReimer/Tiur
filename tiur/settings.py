de1f230f1936f4488f3042eab3d4576075f7115e
Jeremy Reimer <jeremy.reimer@gmail.com>
2023-02-13 21:12:05 +0000

    Removed default secret key, set to environment variable

--- tiur/settings.py
+++ tiur/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*b!am2=ms4r^om_zh+tsv4sa1m2n3t1k6)m8y!jon%q_ns2#w5'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "default_value")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
