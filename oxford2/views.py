dbf3e05b80cc170b434b9f1a4b53e34e146b3ea0
Jeremy Reimer <jeremy.reimer@gmail.com>
2023-02-15 18:59:57 +0000

    Added first template for pages, updated view to use template

--- oxford2/views.py
+++ oxford2/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Project
 
 def index(request):
     return HttpResponse("Hello, world! This is Oxford 2.0's index.")
 
 def pageview(request, page_url):
-    projects_list = Project.objects.order_by('-name')
-    output = "This is the HTML for the %s project." % page_url
-    output = output + "\n" + ', '.join([p.name for p in projects_list])
-    return HttpResponse(output)
+    project_list = Project.objects.order_by('-name')
+    template = loader.get_template('oxford2/index.html')
+    context = {
+       'project_list': project_list,
+       'current_project': page_url,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 
 
