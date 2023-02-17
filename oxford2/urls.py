43278f7b41e3f63d7ee574c21d0fd2846e88b0bf
Jeremy Reimer <jeremy.reimer@gmail.com>
2023-02-17 18:30:02 +0000

    Added dynamic loading of test stub HTML file and displaying on main page

--- oxford2/urls.py
+++ oxford2/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
 #   path('admin/', admin.site.urls),
     path('<page_url>/latest/', views.pageview, name='pageview'),
+#   re_path(r'<page_url>/latest/.*', views.pageview, name='pageview'),
 ]
--- oxford2/views.py
+++ oxford2/views.py
@@ -1,17 +1,26 @@
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.conf import settings
 from .models import Project
 
 def index(request):
     return HttpResponse("Hello, world! This is Oxford 2.0's index.")
 
 def pageview(request, page_url):
-    project_list = Project.objects.order_by('-name')
+    project_list = Project.objects.order_by('name')
     template = loader.get_template('oxford2/index.html')
+    page_url_full = os.path.join(settings.BASE_DIR, 'oxford2', 'artifacts', page_url, 'welcome-to-demonware.html')
+    try: 
+      with open(page_url_full, "r", encoding="utf-8") as f:
+          page_content = f.read() 
+    except:
+      page_content = 'No page found at ' + str(page_url_full)
     context = {
        'project_list': project_list,
        'current_project': page_url,
+       'page_content': page_content,
     }
     return HttpResponse(template.render(context, request))
 
