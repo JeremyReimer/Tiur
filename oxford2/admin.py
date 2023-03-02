4271766211b983e6d60aba56fadf8a9da071c6e2
Jeremy Reimer <jeremy.reimer@gmail.com>
2023-03-02 23:50:34 +0000

    Added "Collect all docs" menu dropdown for projects, with stub function

--- oxford2/admin.py
+++ oxford2/admin.py
@@ -11,7 +11,19 @@ from .models import Config
 admin.site.register(Category)
 admin.site.register(BuildType)
 admin.site.register(ParserType)
-admin.site.register(Project)
+#admin.site.register(Project)
 admin.site.register(NavTreeItem)
 admin.site.register(Version)
 admin.site.register(Config)
+
+@admin.action(description='Collect latest docs for project')
+def collect_docs(modeladmin, request, queryset):
+    print(request)
+    print(queryset)
+    # collect the docs for the current project(s)
+
+class ProjectAdmin(admin.ModelAdmin):
+    project_list = []
+    actions = [collect_docs]
+
+admin.site.register(Project, ProjectAdmin)
