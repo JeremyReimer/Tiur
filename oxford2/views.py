63e20e4d2f3869d6ce7c032d05978593dd1c3287
Jeremy Reimer <jeremy.reimer@gmail.com>
2023-03-09 23:28:20 +0000

    Changed template to load navtree from static file instead

--- oxford2/views.py
+++ oxford2/views.py
@@ -14,15 +14,23 @@ def pageview(request, page_url, page):
     project_list = Project.objects.order_by('name')
     template = loader.get_template('oxford2/index.html')
     page_url_full = os.path.join(settings.BASE_DIR, 'oxford2', 'artifacts', page_url, page)
+    nav_url_full = os.path.join(settings.BASE_DIR, 'oxford2', 'artifacts', 'navtree.html')
     try: 
       with open(page_url_full, "r", encoding="utf-8") as f:
           page_content = f.read() 
     except:
       page_content = 'No page found at ' + str(page_url_full)
+    # load navtree
+    try:
+      with open(nav_url_full, "r", encoding="utf-8") as n:
+          nav_content = n.read()
+    except:
+          nav_content = 'Unable to load navigation tree.'
     context = {
        'project_list': project_list,
        'current_project': page_url,
        'page_content': page_content,
+       'nav_content': nav_content,
     }
     return HttpResponse(template.render(context, request))
 
