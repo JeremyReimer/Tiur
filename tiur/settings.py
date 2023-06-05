1abab3a4e4bc935e0a35fd5157b61b9315e9488d
JeremyReimer <jeremy_reimer@tiur-staging.v1>
2023-06-05 21:18:57 +0000

    Set up Tiur to run on Gunicorn/Nginx web server

--- tiur/settings.py
+++ tiur/settings.py
@@ -132,7 +132,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'oxford2')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/tiur/static'
+STATICFILES_DIRS = [BASE_DIR / "oxford2" / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
